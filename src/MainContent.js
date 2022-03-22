10f4874ecf6225eb75b854ec2e47096b15b5efb1
rohitjainfsl <100214764+rohitjainfsl@users.noreply.github.com>
2022-03-22 20:03:29 +0530

    Add files via upload

---
+++ src/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class MainContent extends React.Component{
+	render(){
+		return(
+			<p>This is the main Section of the website</p>
+		)
+	}
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Counter from './Counter'
+import './index.css'
+
+// JSX - mixture HTML, JS and XML
+
+// const name = 'Parth'
+
+
+// function Hello(props){
+//   // console.log(props);
+//   return(
+//     <h1>Hello {props.name}</h1>
+//   )
+// }
+
+
+
+
+
+
+
+ReactDOM.render(<Counter />, document.querySelector("#root")) 
